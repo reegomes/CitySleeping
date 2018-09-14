9979db75f2c5057735c007474cf2054ebeed2f31
Renato Gomes <reegomescel@gmail.com>
2018-09-14 18:46:23 -0300

    v1

---
+++ Assets/Scripts/DataLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour
+{
+    public string[] users;
+    IEnumerator Start()
+    {
+        WWW ctrlAcess = new WWW("http://localhost/ww/ControlAcess.php");
+        yield return ctrlAcess;
+        string ctrlAcessString = ctrlAcess.text;
+        print(ctrlAcessString);
+        users = ctrlAcessString.Split(';');
+        print(GetDataValue(users[0], "Username: "));
+    }
+    string GetDataValue(string data, string index)
+    {
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+        if (value.Contains("|"))
+        {
+            value = value.Remove(value.IndexOf("|"));
+        }
+        return value;
+    }
+}
--- Assets/Scripts/TurnBasedSystem.cs
+++ Assets/Scripts/TurnBasedSystem.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class TurnBasedSystem : MonoBehaviour {
 	public GameObject SendVotes, Voted, Chat;
 	public TextMeshProUGUI Tretatext;
+	float timer; int timerInt; public Text textTimer;
+
 	void Start () {
 		StartCoroutine(StartRounds());
+		timer = 40;
 	}
 	private void Update() {
 		Tretatext.text = "Está rolando um chat nesse momento, você tem que usar seus argumentos agora.";
+		textTimer.text = "Tempo: " + timerInt.ToString();
+		timer -= Time.deltaTime;
+		timerInt = (int)timer;
 	}
 	public IEnumerator StartRounds(){
 		yield return new WaitForSeconds(40);
 		SendVotes.SetActive(true);
 		StartCoroutine(AwaitingVotes());
+		timer = 15;
 	}
 	public IEnumerator AwaitingVotes(){
 		yield return new WaitForSeconds(15);
 		Chat.SetActive(false);
 		StartCoroutine(CalculatingVotes());
+		timer = 10;
 	}
 	public IEnumerator CalculatingVotes(){
 		yield return new WaitForSeconds(10);
@@ -27,5 +36,6 @@ public class TurnBasedSystem : MonoBehaviour {
 		Voted.SetActive(false);
 		Chat.SetActive(true);
 		StartCoroutine(StartRounds());
+		timer = 40;
 	}
 }
